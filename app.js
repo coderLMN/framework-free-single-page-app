9f4971f2615e42132449930eb3d7e2f841a8bc21
coderLMN <jinzhaowu@yahoo.com>
2015-01-15 16:57:07 +0800

    package to be a lib

---
+++ app.js
@@ -0,0 +1,32 @@
+//The following is customizable, and consistent to the templates used
+var postMD = {};
+postMD.partial = "postMD.html";
+postMD.init = function(){
+    miniSPA.render('postMD');             //render related partial page
+}
+postMD.submit = function(){
+    document.getElementById('spinner').style.visibility = 'visible';
+    var mdText = document.getElementById('mdText');
+    var md = document.getElementById('md');
+    var data = '{"text":"'+mdText.value.replace(/\n/g, '<br>')+'","mode": "gfm","context": "github/gollum"}';
+    miniSPA.ajaxRequest('https://api.github.com/markdown', 'POST', data,function(status, page){
+        document.getElementById('spinner').style.visibility = 'hidden';
+        md.innerHTML = page;     //render markdown partial returned from the server
+    });
+    mdText.value = '';
+}
+
+var getEmoji = {};
+getEmoji.partial = "getEmoji.html"
+getEmoji.init = function(){
+    document.getElementById('spinner').style.visibility = 'visible';
+    document.getElementById('content').style.visibility = 'hidden';
+    miniSPA.ajaxRequest('https://api.github.com/emojis','GET','',function(status, partial){
+        getEmoji.emojis = JSON.parse(partial);
+        miniSPA.render('getEmoji');       //render related partial page with data returned from the server
+        document.getElementById('content').style.visibility = 'visible';
+        document.getElementById('spinner').style.visibility = 'hidden';
+    });
+}
+
+miniSPA.changeUrl();    //initialize
